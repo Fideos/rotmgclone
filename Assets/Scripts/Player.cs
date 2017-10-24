c5af5ef775f0990dcb1510ab91d293e6491ec58e
Mauricio Damian Salazar <mdamiansalazar@gmail.com>
2017-10-24 13:54:36 -0300

    Initial Commit.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    private float speed = 10f;
+    private float inputX;
+    private float inputY;
+    Vector3 vectorDirector;
+    Vector3 targetPosition;
+
+
+    void Start () {
+
+        
+
+    }
+	
+	void Update () {
+
+
+        inputX = Input.GetAxis("Side");
+        inputY = Input.GetAxis("UpDown");
+        vectorDirector = new Vector3(inputX, inputY).normalized;
+        targetPosition = transform.position + vectorDirector * speed;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+
+
+
+
+    }
+}
