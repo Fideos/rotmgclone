7f4f80c00db310a743b538feda7fa0fef2d0cb59
Mauricio Damian Salazar <mdamiansalazar@gmail.com>
2017-12-04 03:40:49 -0300

    Implementé script de disparo de bullet (A medias).

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
 
+    private float speed = 100f;
+    public GameObject bullet;
+    GameObject bulletClone;
 
-	void Start () {
-		
-	}
-	
+    public Transform origin;
+    Vector3 target;
 
-	void Update () {
-		
-	}
+    void ShootBullet()
+    {
+        target = Input.mousePosition;
+        target = Camera.main.ScreenToWorldPoint(target) - transform.position;
+        target.z = 0;
+        bulletClone = Instantiate(bullet, origin.position, origin.rotation) as GameObject;
+        Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D>();
+        rb.AddForce(target * speed);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootBullet();
+        }
+    }
 
 }
