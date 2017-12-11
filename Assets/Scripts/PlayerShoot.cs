4c19aa0a698f1e32a7b4490571e5592c889acb8e
Mauricio Damian Salazar <mdamiansalazar@gmail.com>
2017-12-11 01:42:35 -0300

    Implementé bala especial.

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
 
     private float speed = 1200f;
+    public float specialBonusSpeed;
     public GameObject bullet;
+    public GameObject specialBullet;
     GameObject bulletClone;
     public AudioClip shootSound;
     public Transform origin;
+    public float specialBulletCooldown;
     Vector3 target;
     private AudioSource source;
+    bool specialBulletAvailable;
 
     // Reproduce el audio en 2 distintos volumenes aleatoriamente.
     private float volLowRange = .5f;
     private float volHighRange = 1.0f;
 
-    void ShootBullet()
+    IEnumerator Yielder() // CoolDown de la bala especial
+    {
+        Debug.Log("Cargando bala especial...");
+        yield return new WaitForSeconds(specialBulletCooldown);
+        specialBulletAvailable = false;
+        Debug.Log("¡Bala especial cargada!");
+    }
+
+    void ShootBullet(GameObject typeOfBullet, float bonusSpeed)
     {
         float vol = Random.Range(volLowRange, volHighRange);
         source.PlayOneShot(shootSound, vol);
         target = Input.mousePosition;
         target = Camera.main.ScreenToWorldPoint(target) - transform.position;
         target.z = 0;
-        bulletClone = Instantiate(bullet, origin.position, origin.rotation) as GameObject;
+        bulletClone = Instantiate(typeOfBullet, origin.position, origin.rotation) as GameObject;
         Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D>();
-        rb.AddForce(target.normalized * speed);
+        rb.AddForce(target.normalized * bonusSpeed);
         
     }
 
@@ -39,7 +51,16 @@ public class PlayerShoot : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            ShootBullet();
+            ShootBullet(bullet, speed + 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (!specialBulletAvailable)
+            {
+                ShootBullet(specialBullet, speed + specialBonusSpeed);
+                specialBulletAvailable = true; // Si es seteado a true, impide que la bala sea disparada.
+                StartCoroutine("Yielder");
+            }
         }
     }
 
---
+++ Assets/Scripts/SpecialBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialBullet : MonoBehaviour {
+
+    public float lifeTime; //Determina tiempo de vida de la bullet
+    public Rigidbody2D rb;
+
+    IEnumerator Yielder() // Eliminar este destructor despues de implementar object pool.
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+        StartCoroutine("Yielder");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 720 * Time.deltaTime);
+    }
+}
+
